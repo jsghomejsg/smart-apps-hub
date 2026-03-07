d7995b5dfffc1d8e0bac387412b0ee3aa2ede94e
jsghomejsg <jsghomejsg@gmail.com>
2026-03-07 09:52:19 +0100

    Update sw.js

--- app102-evalua-andalucia/sw.js
+++ app102-evalua-andalucia/sw.js
@@ -1,24 +1,8 @@
 const CACHE_NAME = 'evalua-v1';
-const assets = [
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;400;700;800&display=swap'
-];
-
-// Instalar el Service Worker y guardar archivos en caché
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+const assets = ['./index.html', './curriculum.json', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
-
-// Responder con la caché si no hay internet
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
